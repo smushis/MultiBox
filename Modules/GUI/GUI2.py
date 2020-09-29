0df9914c928353e9ff99a61472340417a0be2920
Smushis <boyer.norman@gmail.com>
2020-09-29 21:33:21 +0200

    trying things

--- Modules/GUI/GUI2.py
+++ Modules/GUI/GUI2.py
@@ -872,17 +872,17 @@ class Ui_MainWindow(object):
         im_conv = self.convert_to_srgb(im)
         im_conv.save("img/Twitter/" + data["username"] + ".png")
         Photo.setPixmap(QtGui.QPixmap("img/Twitter/" + data["username"] + ".png"))
-        self.Twitch_Title.setHidden(True)
+        self.Twitch_Title.setText("")
         if data["media"] != None:
             self.getImage(data["media"]["link"], data["media"]["id"], "Twitter_Media", 275)
             self.media.setPixmap(QtGui.QPixmap("img/Twitter_Media/" + data["media"]["id"] + ".png"))
             self.media.setVisible(True)
         if data["events"] == "Mention":
-            cadre.setPixmap(QtGui.QPixmap("img/GUI2/youtube.png"))
+            cadre.setPixmap(QtGui.QPixmap("img/GUI2/tweet_mentions.png"))
         elif data["events"] == "rt":
-            cadre.setPixmap(QtGui.QPixmap("img/GUI2/youtube.png"))          
+            cadre.setPixmap(QtGui.QPixmap("img/GUI2/tweet_RT.png"))          
         elif data["events"] == "fav":
-            cadre.setPixmap(QtGui.QPixmap("img/GUI2/youtube.png"))
+            cadre.setPixmap(QtGui.QPixmap("img/GUI2/tweet_like.png"))
             
     def printStreams(self, data, label, Photo, title, cadre):
         self.media.setHidden(True)
